3c5b376a5185e075cee333aa180121cb27c64832
Thibault-21 <thib.dec.pro@gmail.com>
2020-02-20 10:54:45 +0100

    first commit

---
+++ src/components/Content/Content.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="main">
+
+  </div>
+</template>
+
+<script>
+// import axios from 'axios';
+export default {
+
+}
+</script>
+ 
+<style scoped>
+
+</style>
---
+++ src/components/Header/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="main">
+    <h3>Kata JS</h3>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style scoped>
+.main {
+  background-color: rgb(221, 36, 11)
+}
+</style>
